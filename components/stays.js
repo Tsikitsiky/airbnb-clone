dac7ea72eac5301716a9c623e9d7cb3ed9a527ae
Sarah Razanatsoa <sarah.raz@onja.org>
2020-10-27 11:39:45 +0300

    change the condition to shoe=w the beds number

--- components/stays.js
+++ components/stays.js
@@ -6,7 +6,7 @@ function Stays(apart) {
             <img className="photo" src={apart.photo} alt={apart.title} />
             <div>
                 {apart.superHost? <button className='super-host'>Super Host</button> : '' }
-                <p className="grey-text">{apart.type} {apart.superHost ? `.${apart.beds} beds` : ''} </p>
+                <p className="grey-text">{apart.type} .{apart.beds && `${apart.beds} beds`} </p>
                 <p className="rating">{apart.rating}</p>
             </div>
             <p>{apart.title}</p>
